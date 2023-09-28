8aa59f7fa45d10943b269d2b170b6b11ce5983cf
Furtuna-Mariana <furtuna.mariana@gmail.com>
2023-09-28 21:59:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const numb = 44;
+const str = 'un string';
+const bool = false;
+const undef = undefined;
+const faraValoare = null;
+const obj = {};
+
+console.log(typeof numb)
+console.log(typeof str)
+console.log(typeof bool)
+console.log(typeof undef)
+console.log(typeof faraValoare)
+console.log(typeof obj)
+
+
+const numar1 = 10;
+const numar2 = 20;
+const numar3 = 30;
+
+const suma = numar1 + numar2 + numar3;
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,30 @@
+const numar1 = 10;
+const numar2 = 20;
+const numar3 = 30;
+
+const suma = numar1 + numar2 + numar3;
+
+console.log("numar1 este egal cu numar2:", numar1 === numar2);
+console.log("numar2 este egal cu numar3:", numar2 === numar3);
+console.log("numar1 este mai mare decat numar2:", numar1 > numar2);
+console.log("numar2 este mai mare decat numar3:", numar2 > numar3);
+console.log("numar1 este mai mare sau egal decat numar2:", numar1 >= numar2);
+console.log("numar2 este mai mare sau egal decat numar3:", numar2 >= numar3);
+console.log("numar1 este mai mic decat numar2:", numar1 < numar2);
+console.log("numar2 este mai mic decat numar3:", numar2 < numar3);
+console.log("numar1 este mai mic sau egal decat numar2:", numar1 <= numar2);
+console.log("numar2 este mai mic sau egal decat numar3:", numar2 <= numar3);
+
+const str1 = 'Hello';
+const str2 = 'World';
+
+console.log(str1 + str2)
+
+
+const obj = {
+    nume: 'Furtuna',
+    prenume: 'Mariana',
+    age: '31'
+}
+
+console.log(obj)
